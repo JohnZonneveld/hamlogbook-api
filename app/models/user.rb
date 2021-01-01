2e7bb933a2a9b2cd179143e26336c2e7b0b24959
JohnZonneveld <jjczon@gmail.com>
2021-01-01 10:50:31 -0600

    corrected validation user on edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	#Validations
 	validates_presence_of :callsign, :email, :password_digest, :my_qth
 	validates :callsign, :email, uniqueness: true
-	validates_format_of :my_qth, with: /\A([A-R]{2}[0-9]{2})\z|\A([A-R]{2}[0-9]{2}[a-x]{2})\z|\A([A-R]{2}[0-9]{2}[a-x]{2}[0-9]{2})\z/, on: :create
+	validates_format_of :my_qth, with: /\A([A-R]{2}[0-9]{2})\z|\A([A-R]{2}[0-9]{2}[a-x]{2})\z|\A([A-R]{2}[0-9]{2}[a-x]{2}[0-9]{2})\z/, on: [:create, :update]
 	# After vaidation remove password_digest error message
 	after_validation { self.errors.messages.delete(:password_digest) }
 	  
